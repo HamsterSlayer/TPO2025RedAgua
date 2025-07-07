961f22dae6175bb7172e4c49b77e13b5ade8a2bc
yo <agustinuma20@gmail.com>
2025-07-07 16:02:17 -0300

    Merge origin/master
    # Conflicts:
    #	src/main/java/lineales/dinamicas/Cola.java

---
+++ src/main/java/com/mycompany/reddistribucionagua2025/Ciudad.java
@@ -0,0 +1,22 @@
+package com.mycompany.reddistribucionagua2025;
+
+public class Ciudad {
+    
+    //Atributos ---------------------------------------------------------------
+    private String nombre; 
+    private int cantHabitantes;
+    private String nomenclatura;
+    private float superficie;
+    private float consumoPromedio;
+    
+    //Constructor --------------------------------------------------------------
+    public Ciudad(String nombre, int cantHabitantes, float superficie, float consumoPromedio)   {
+        this.nombre = nombre;
+        this.cantHabitantes = cantHabitantes;
+        this.superficie = superficie;
+        this.consumoPromedio = consumoPromedio;
+        //this.nomenclatura = calcularNomenclatura();
+    }
+    
+    //Metodos ---------------------------------------------------------------
+}
