a48821f5e414f93b478f9ee2ec45a47f7d7f197b
yo <agustinuma20@gmail.com>
2025-07-07 14:07:11 -0300

    Merge origin/master
    # Conflicts:
    #	src/main/java/lineales/dinamicas/Cola.java

---
+++ src/main/java/com/mycompany/reddistribucionagua2025/RedDistribucionAgua2025.java
@@ -0,0 +1,12 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.reddistribucionagua2025;
+
+/**
+ *
+ * @author hamst
+ */
+public class RedDistribucionAgua2025 {
+}
