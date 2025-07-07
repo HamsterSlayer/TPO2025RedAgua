8a815604889c5813c643e87042e72af098381230
ivoel <ivoel@PCIVO>
2025-07-07 14:40:17 -0300

    Fases muy tempranas del menu

--- src/main/java/com/mycompany/reddistribucionagua2025/TransporteDeAgua.java
+++ src/main/java/com/mycompany/reddistribucionagua2025/TransporteDeAgua.java
@@ -1,13 +1,23 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.mycompany.reddistribucionagua2025;
-
+import java.util.Scanner;
 /**
  *
  * @author ivoel
  */
 public class TransporteDeAgua {
+    
+    //Menu
+    Scanner sc = new Scanner(System.in);
+    byte opcion;
+    opcion = sc.nextInt();
+    
+    switch opcion{
+        
+        8:
+            
+        break;
+        default:
+            System.out.println("Opcion erronea");
+    }
     
 }
